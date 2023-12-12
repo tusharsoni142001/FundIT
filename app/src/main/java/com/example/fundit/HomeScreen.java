ecdc115187a1374c03196c2754c9011b489306c7
Tushar SOni <tusharsoni142001@gmail.com>
2023-12-12 20:07:31 +0530

    Splash screen and home screen

---
+++ app/src/main/java/com/example/fundit/HomeScreen.java
@@ -0,0 +1,63 @@
+package com.example.fundit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeScreen extends AppCompatActivity {
+    Button signup, signin;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        Toast.makeText(getApplicationContext(),"HomeScreen",Toast.LENGTH_LONG).show();
+
+        // Initialize Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        signup = findViewById(R.id.btn_signup);
+        signin = findViewById(R.id.btn_signin);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.d("User UID", currentUser.getUid());
+            Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+            startActivity(intent);
+            finish(); // Close the current activity
+
+        }
+    }
+
+}
