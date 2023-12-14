6fbe51cd6341552d7e702f0f211991d06c1c23c3
Abhi <abhispykar1812@gmail.com>
2023-12-15 01:17:31 +0530

    Founder for and ui companty form design

---
+++ app/src/main/java/com/example/fundit/founderform.java
@@ -0,0 +1,51 @@
+package com.example.fundit;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class founderform  extends AppCompatActivity {
+    private EditText email, name, about;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.founderform);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Founder");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // initialising the layout items
+        email = findViewById(R.id.et_email);
+        name = findViewById(R.id.et_name);
+    }
+
+
+    }
