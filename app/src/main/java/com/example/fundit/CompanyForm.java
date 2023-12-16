dddd0a3ade2a64aef8a0f306777ab85ca6f05f6b
Abhi <abhispykar1812@gmail.com>
2023-12-16 14:52:31 +0530

    CompanyForm changes done

--- app/src/main/java/com/example/fundit/CompanyForm.java
+++ app/src/main/java/com/example/fundit/CompanyForm.java
@@ -3,12 +3,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CompanyForm extends AppCompatActivity {
+    TextView companName, foundedyear;
+    EditText Contact,AbtCompany;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.companyform);
+
+        companName = findViewById(R.id.companyName);
+        foundedyear = findViewById(R.id.foundedyear);
+        Contact = findViewById(R.id.userContact);
+        AbtCompany = findViewById(R.id.abtcompany);
+
+
+
     }
 }
